74a41ae8ed947374acaaa992e4a36ca575a8d8c0
ismamaster23 <ismagui.2001@hotmail.com>
2025-07-05 14:16:17 -0600

    modificacion de rutas pacientes 2

--- Fronted/src/views/patients/NewPatient.jsx
+++ Fronted/src/views/patients/NewPatient.jsx
@@ -224,7 +224,7 @@ const NewPatient = () => {
     const handleDialogClose = () => {
         setDialogOpen(false);
         if (dialogTitle === 'Éxito') {
-            window.location.href = 'https://pruebas-back-50777bb3ad67.herokuapp.com/GenerarDietas', {
+            navigate('https://pruebas-back-50777bb3ad67.herokuapp.com/GenerarDietas', {
                 state: {
                     correo: formData.correo,
                     peso: formData.peso,
